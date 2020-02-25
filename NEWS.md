30f4fe034ca252ddc34349047ddf1a698c40cf4a
msperlin <marcelperlin@gmail.com>
2020-02-25 12:04:07 -0300

    commit decd6c0c8a

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,7 @@
+## Version 0.2.1 (2020-02-25)
+
+- Improved startup message for 2020 book (link to online version)
+
 ## Version 0.2.0 (2020-02-13)
 
 - Added startup message for 2020 book
--- R/onAttach.R
+++ R/onAttach.R
@@ -1,15 +1,18 @@
 .onAttach <- function(libname,pkgname) {
 
-  do_color <- crayon::make_style("#18bc9c")
+  do_color <- crayon::make_style("#FF4141")
+  this_pkg <- 'simfinR'
 
   if (interactive()) {
-    msg <- paste0('\nHi ', Sys.getenv('USER'), '!\n',
-                  'Want to learn more about using R in Finance and Economics? ',
-                  'The second edition (2020) of my R book is freely available at ',
-                  do_color('https://www.msperlin.com/afedR/'))
+    msg <- paste0('\nWant to learn more about ',
+                  do_color(this_pkg), ' and other R packages for Finance and Economics?',
+                  '\nThe second edition (2020) of ',
+                  do_color('Analyzing Financial and Economic Data with R'), ' is available at\n',
+                  do_color('https://www.msperlin.com/afedR/') )
   } else {
     msg <- ''
   }
+
   packageStartupMessage(msg)
 
 }
